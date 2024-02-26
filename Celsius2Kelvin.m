a567535ce8066b76cbedfa438e38e558d70bbb89
ajskilton <161122279+ajskilton@users.noreply.github.com>
2024-02-26 14:33:55 +1300

    Delete Celsius2Kelvin.m

--- Celsius2Kelvin.m
+++
@@ -1,8 +0,0 @@
-function [kel] = Celsius2Kelvin(cels)
-%CELSIUS2KELVIN converts tempreture in degrees celsius to kelvin.
-%INPUTS:
-%   cels : double value of tempreture in degree celsius
-%OUTPUTS:
-%   kel: temp in kelvin after convertion
-kel = cels + 273.15;
-end
